85f216cdfa3a6df5e556048d474fc77dec626f96
somya-ctrl <agarwalsomya224@gmail.com>
2026-06-29 00:29:49 +0530

    login

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,8 +1,8 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 import "dotenv/config";
 
-export const prisma = new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL,
-});
-
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+export const prisma = new PrismaClient({ adapter });
 export default prisma;
+
--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -5,5 +5,5 @@ import { signupSchema,loginSchema} from "../validators/auth.validator";
 
 const router = Router();
 router.post("/signup",validate(signupSchema),authController.signup);
-router.post("./login",validate(loginSchema),authController.login);
+router.post("/login",validate(loginSchema),authController.login);
 export default router;
